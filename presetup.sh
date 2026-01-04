8a39a0a01f9b2fd6de0a2c63f9caf363b7dbd274
Domen Banfi <banfidomen@gmail.com>
2026-01-04 17:10:51 +0100

    Working commit

--- presetup.sh
+++ presetup.sh
@@ -3,6 +3,9 @@
 sudo apt update
 
 ###################### P1 ###############
+# Install curl
+sudo apt install -y curl
+
 # Add HashiCorp GPG Key and Repository for vagrant and isntall vagrant
 if ! command -v vagrant &> /dev/null; then
     curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
@@ -19,8 +22,6 @@ sudo apt install -y virtualbox
 sudo apt install -y ansible
 
 #################### P2 ################
-# Install curl
-sudo apt install -y curl
 
 # Install kubectl
 if ! command -v kubectl &> /dev/null; then
@@ -58,7 +59,7 @@ fi
 
 # Install Docker and prequisits (prerequisite for K3d)
 if ! command -v docker &> /dev/null; then
-    sudo apt install -y ca-certificates curl gnupg lsb-release
+    sudo apt install -y ca-certificates gnupg lsb-release
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
